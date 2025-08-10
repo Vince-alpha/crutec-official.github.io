9e88a98a5ae841863865b70da88ccc593e68676f
Namkung Whee Yoon <vincenamkung@soongsil.ac.kr>
2025-08-10 18:17:39 +0900

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-
-let idx=0;
-const slides=()=>document.querySelectorAll('.hero .slide');
-function show(i){slides().forEach((el,ix)=>el.classList.toggle('active',ix===i));}
-function next(){idx=(idx+1)%slides().length;show(idx);}
-show(0);
-setInterval(next,5000);
